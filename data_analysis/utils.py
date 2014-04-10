8c3bfe9aee1fa365ab7b778210255f0c75a72042
rebel <rebel@retresco.de>
2014-04-10 22:48:16 +0200

    nan, l2n, timespan ..

--- data_analysis/utils.py
+++ data_analysis/utils.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # encoding: utf-8
+from string import letters
+from dateutil import parser
+
 
 def normalize_people_age(age):
     """
@@ -7,6 +10,7 @@ def normalize_people_age(age):
         may better by hand [21, 31, 51, 71, 100] ...we will see
     """
     # for g in range(16, 100, 20):
+    age = int(age)
     for g in [25, 40, 60, 100]:
         if age < g:
             return g    
@@ -15,6 +19,7 @@ def normalize_people_age(age):
 def normalize_car_age(age):
     """
     """
+    age = int(age)
     for g in [2, 4, 8, 16, 32, 64, 128]:
         if age < g:
             return g    
@@ -28,6 +33,34 @@ def step_reduce(counter, steps, step):
     else:
         steps.append(step)
     return counter + 1, steps
+
+
+NAN_VALUES = {
+    'C_previous': 5,
+    'duration_previous': 16,
+    'risk_factor': 5,
+    'car_value': 'z',
+    'location': 55555
+    }
+
+def nan(value, name):
+    try:
+        value = int(value)
+    except:
+        if value == '' or value == 'NA':
+            value = NAN_VALUES[name]                
+    return value
+
+
+def l2n(value):
+    number = ''.join([str(letters.index(l)) for l in list(value)])   
+    return number
+
+
+def timespan(first, second):
+    time1 = parser.parse(first)
+    time2 = parser.parse(second)
+    return (time2-time1).seconds/60 
 
 
 def print_facets(field, response, tab=1):    
