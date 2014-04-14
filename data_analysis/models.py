8e54568cc5c14566998bec0f4e23d2e00554c4ca
rebel <rebel@retresco.de>
2014-04-15 01:03:49 +0200

    model and query separated

--- data_analysis/models.py
+++ data_analysis/models.py
@@ -3,40 +3,6 @@
 from utils import normalize_people_age, normalize_car_age
 from utils import nan, l2n
 
-
-
-
-class Query(object):
-
-    query = {"size": 0}
-    # no query given is like default {"match_all" : {}}
-
-    def add_facet(self, key, field, size=10):
-        self.query["facets"] = {key: {"terms": {"field": field, "size": size}}}
-
-
-
-    # def add_aggs(self, key, field):
-    #     self.query["aggregations"] = item['{key: {"sum": {"field": option}}}
-
-    def add_query(self, a_query):
-        self.query["query"] = a_query
-
-    def reset_query(self):
-        self.query = {"size": 0}
-
-    def the_query(self):
-        return self.query
-
-
-class QueryResponse(object):
-
-    def __init__(self, arg):        
-        self.response = arg
-
-    def get_facets(self, key):
-        return self.response["facets"][key]["terms"]
-
         
 class Item(object):
     def __init__(self, line, field_names):
@@ -138,12 +104,4 @@ class User(object):
         # check the 'coverage options', check if user switch his data around
         # and what in each step
         # represent the reduced steps ...short path kind of 
-
-
-
-
-
-
-
-
 
